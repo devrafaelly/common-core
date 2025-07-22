aced5c7c9e83d742f7be3e339e741ac25f69cfa7
Rafaelly Diniz <rafaellypoliveira@gmail.com>
2025-07-22 20:57:20 -0300

    style: ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -12,6 +12,8 @@
 
 #include "libft.h"
 
+#include <string.h>
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
@@ -19,11 +21,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && (*s1 || *s2))
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
-		s1++;
-		s2++;
 	}
 	return (0);
 }
