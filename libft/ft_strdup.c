90529efe927ee71bec8cb53c85f60a3e82640d3c
Rafaelly Diniz <138537427+devrafaelly@users.noreply.github.com>
2025-07-25 22:52:36 +0000

    fix: updating functions

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rafaoliv <rafaoliv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 16:10:10 by rafaoliv          #+#    #+#             */
-/*   Updated: 2025/07/24 16:46:00 by rafaoliv         ###   ########.fr       */
+/*   Updated: 2025/07/25 21:47:19 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,4 @@ char	*ft_strdup(const char *s)
 	}
 	s_cpy[i] = '\0';
 	return (s_cpy);
-	free(s_cpy);
 }
